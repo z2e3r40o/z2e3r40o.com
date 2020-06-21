b89c0be86fa89bc3ce5e43fbeeaf7da99132d8a6
z2e3r40o <z2e3r40l@protonmail.com>
2020-06-21 12:28:46 -0700

    gh-pages -> master

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -12,7 +12,7 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
+echo "Checking out master branch into public"
 git worktree add -B master public origin/master
 
 echo "Removing existing files"
@@ -21,8 +21,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
 
 #echo "Pushing to github"
 #git push --all
