a900b0199823cc141478994f54435e1e0701c4d2
z2e3r40o <z2e3r40l@protonmail.com>
2020-06-21 12:32:47 -0700

    cp cname

--- publish_to_master.sh
+++ publish_to_master.sh
@@ -21,6 +21,9 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+echo "Copy CNAME"
+cp CNAME public/CNAME
+
 echo "Updating master branch"
 cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
 
